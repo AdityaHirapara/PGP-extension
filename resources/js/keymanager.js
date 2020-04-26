56287433190f9cf62b5467e372682d7fd7ed11a7
AdityaHirapara <adityahirapara2016@gmail.com>
2020-04-26 16:46:15 +0530

    Form Validations

--- resources/js/keymanager.js
+++ resources/js/keymanager.js
@@ -2,3 +2,47 @@ function keyshow(id) {
   $('.keymodule').hide();
   $(`#${id}`).show();
 }
+
+$('#genkeyform')
+  .form({
+    fields: {
+      genname: {
+        identifier: 'genname',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your name'
+          }
+        ]
+      },
+      genmail: {
+        identifier: 'genmail',
+        rules: [
+          {
+            type   : 'email',
+            prompt : 'Please enter valid email'
+          }
+        ]
+      },
+      genpassphrase: {
+        identifier: 'genpassphrase',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your passphrase'
+          }
+        ]
+      },
+    }
+  })
+;
+
+function generateKeys() {
+  if( $('#genkeyform').form('is valid')) {
+    const name = $('input[name="genname"]').val();
+    const email = $('input[name="genmail"]').val();
+    const passphrase = $('input[name="genpassphrase"]').val();
+
+    // openpgp generate key function
+  }
+}
