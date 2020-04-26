515d73826c4cec393216a0c8fdc0029f5efbbdd6
Nirav <niravtalaviya011@gmail.com>
2020-04-26 21:52:55 +0530

    index and keymanager file updated for "find and import key"

--- resources/js/keymanager.js
+++ resources/js/keymanager.js
@@ -50,18 +50,10 @@ function generateKeys() {
 
 
 //************************************Nirav start****************************************//
+
 $('#findkeyform')
   .form({
     fields: {
-      findname: {
-        identifier: 'findname',
-        rules: [
-          {
-            type   : 'empty',
-            prompt : 'Please enter your name'
-          }
-        ]
-      },
       findmail: {
         identifier: 'findmail',
         rules: [
@@ -74,16 +66,96 @@ $('#findkeyform')
     }
   })
 ;
+$('#importkeyform')
+  .form({
+    fields: {
+      importpublickey: {
+        identifier: 'importpublickey',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter valid PublicKey'
+          }
+        ]
+      },
+    }
+  })
+;
+function storeKey(name, email, pubKey) {
+	var details = {};
+	details[email] = {
+		"name": name,
+		"pubKey": pubKey
+	};
+	chrome.storage.local.set(details, function() {
+		console.log("Stored key at", email);
+		window.location.reload();
+	});
+}
 
 function findKey() {
-  console.log('Clicked on find-key');
-  if( $('#findkeyform').form('is valid')) {
-    const name = $('input[name="findname"]').val();
-    const email = $('input[name="findmail"]').val();
-    console.log(name);
-	console.log(email);
-    // openpgp find key function
-  }
+	console.log('Clicked on Find button')
+	if( $('#findkeyform').form('is valid')) {
+		var email = 'mahishmatiradadiya1290347856@kotmail.com'
+		var email = $('input[name="findmail"]').val();
+		var hkp = new openpgp.HKP('https://keyserver.ubuntu.com/');
+		var options = {
+			query: email 
+		};
+		hkp.lookup(options).then(async function(key) {
+			var publicKey = await openpgp.key.readArmored(key);
+			var userid = publicKey['keys'][0]['users'][0].userId['userid'].split(' ');
+			var name='';
+			for (i = 0; i < userid.length-1; i++) {
+				name += userid[i] + " ";
+			}
+			storeKey(name, email, key);
+		});
+	}
+}
+
+
+function handleFileSelect(evt) {
+    var i,files = evt.target.files[0]; // FileList object
+	var reader = new FileReader();
+	reader.onload = (function(theFile) {
+		return function(e) {
+		  var span = document.createElement('span');
+		  span.innerHTML = ['<textarea class="ui message" disable>'+e.target.result+'</textarea>'].join('');
+		  document.getElementById('importpublickey').innerHTML=e.target.result;
+		};
+	})(files);
+	reader.readAsText(files);
+}
+
+document.getElementById('files').addEventListener('change', handleFileSelect, false);
+  
+  
+async function importKey() {
+	console.log('Clicked on Import Public Key button')
+	if( $('#importkeyform').form('is valid')) {
+		var publickey = $.trim($("textarea").val());
+		verifypubandsave(publickey);
+	}
+}
+
+function verifypubandsave(pubkey){
+	try{
+		var pubKey = openpgp.key.readArmored(pubkey);
+		var userid = pubKey['keys'][0]['users'][0].userId['userid'];
+		userid = userid.split(' ');
+		var name='';
+		for (i = 0; i < userid.length-1; i++) {
+			name += userid[i] + " ";
+		}
+		email = userid[userid.length-1];
+		console.log('Name from public key: ',name);
+		console.log('Email from public key: ',email);
+		storeKey(name, email, key);
+		alert('Your Public Key stored Successfully');
+	}catch(err){
+		alert('Invalid Public Key');
+	}
 }
 
 // ************************************Nirav End******************************************//
