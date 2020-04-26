8abc54b79cff9f11c1e51182a4a38738676f08c7
AdityaHirapara <adityahirapara2016@gmail.com>
2020-04-26 21:52:44 +0530

    Implement generate key module

--- resources/js/keymanager.js
+++ resources/js/keymanager.js
@@ -3,6 +3,10 @@ function keyshow(id) {
   $(`#${id}`).show();
 }
 
+$('#genkeyTab').click(keyshow.bind(this, 'genkey'));
+$('#findkeyTab').click(keyshow.bind(this, 'find'));
+$('#managekeyTab').click(keyshow.bind(this, 'manage'));
+
 $('#genkeyform')
   .form({
     fields: {
@@ -37,12 +41,61 @@ $('#genkeyform')
   })
 ;
 
+$("#gensubmit").click(generateKeys);
+
+function newKeyPair(name, email, password) {
+	var options = {
+		userIds: [{
+			name: name,
+			email: email
+		}],
+		numBits: 4096,
+		passphrase: password
+	}
+
+	openpgp.generateKey(options).then(function(key) {
+		console.log("Generated key pair");
+		storeKeyPair(name, email,
+					 key.publicKeyArmored,
+					 key.privateKeyArmored);
+	});
+}
+
+function storeKeyPair(name, email, pubKey, privKey) {
+	var details = {};
+	details[email] = {
+		"name": name,
+		"pubKey": pubKey,
+		"privKey": privKey
+	};
+	chrome.storage.local.set(details, function() {
+		console.log("Stored key pair at", email);
+		$('#keysendmodal').modal({
+      onApprove : function() {
+        const hkp = new openpgp.HKP('https://keyserver.ubuntu.com');
+        hkp.upload(pubKey).then(function(l) { 
+          console.log(l)
+          var options = {
+            query: email
+          };
+        
+          hkp.lookup(options).then(async function(key) {
+              var pub = await openpgp.key.readArmored(key);
+              console.log(key);
+              console.log(pub);
+          });
+        });
+      }
+    }).modal('show');
+	});
+}
+
 function generateKeys() {
   if( $('#genkeyform').form('is valid')) {
     const name = $('input[name="genname"]').val();
     const email = $('input[name="genmail"]').val();
     const passphrase = $('input[name="genpassphrase"]').val();
 
-    // openpgp generate key function
+    newKeyPair(name, email, passphrase);
   }
 }
